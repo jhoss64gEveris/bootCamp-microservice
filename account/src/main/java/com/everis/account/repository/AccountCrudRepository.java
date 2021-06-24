033b148aed30e18dd318adae028cd01796652049
jhoss64gEveris <85947496+jhoss64gEveris@users.noreply.github.com>
2021-06-24 17:41:47 -0500

    CONFIG-DOCKER-FUNCIONA

--- account/src/main/java/com/everis/account/repository/AccountCrudRepository.java
+++ account/src/main/java/com/everis/account/repository/AccountCrudRepository.java
@@ -1,7 +1,6 @@
 package com.everis.account.repository;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.everis.account.domain.Account;
@@ -9,11 +8,10 @@ import com.everis.account.domain.Account;
 import reactor.core.publisher.Mono;
 
 @Repository
-public interface AccountCrudRepository extends ReactiveMongoRepository<Account, String>, 
-		ReactiveCrudRepository<Account, String> {
-	
+public interface AccountCrudRepository extends ReactiveMongoRepository<Account, String> {
+
 	public Mono<Account> findByNumberAccount(String numberAccount);
 
 	public Mono<Void> deleteByNumberAccount(String numberAccount);
-	
+
 }
