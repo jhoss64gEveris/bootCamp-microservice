ec72b15468191d10111edcf202cb342c1dd7e34d
jhoss64gEveris <85947496+jhoss64gEveris@users.noreply.github.com>
2021-06-24 14:37:28 -0500

    config-docker image

--- ZuulService/src/main/java/com/everis/ZuulService/ZuulServiceApplication.java
+++ ZuulService/src/main/java/com/everis/ZuulService/ZuulServiceApplication.java
@@ -2,10 +2,10 @@ package com.everis.ZuulService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulServiceApplication {
